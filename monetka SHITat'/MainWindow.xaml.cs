aa4d2da0dd9bd1d884d0d70e601f073057b99eaf
tihon <tihon@DESKTOP-ACP6SLU>
2022-02-04 22:43:08 +0200

    Добавьте файлы проекта.

---
+++ monetka SHITat'/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace monetka_SHITat_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(NominalKypyr.Text);
+            double y = double.Parse(KolvoKypyr.Text);
+            double a = x * y;
+            Result.Text = $"Сумма купюр равна: {a}";
+        }
+    }
+}
